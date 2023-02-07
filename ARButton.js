47437469f59af30e9a27247c4081b5d8e09670a8
Arun kumar <arunscbe@gmail.com>
2023-02-07 15:38:39 +0530

    AR-Button color change

--- ARButton.js
+++ ARButton.js
@@ -148,10 +148,10 @@ class ARButton {
 			element.style.position = 'absolute';
 			element.style.bottom = '20px';
 			element.style.padding = '12px 6px';
-			element.style.border = '1px solid #fff';
+			element.style.border = '1px solid #000';
 			element.style.borderRadius = '4px';
 			element.style.background = 'rgba(0,0,0,0.1)';
-			element.style.color = '#fff';
+			element.style.color = '#000';
 			element.style.font = 'normal 13px sans-serif';
 			element.style.textAlign = 'center';
 			element.style.opacity = '0.5';
